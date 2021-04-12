7bc51f519dcb64d7f3c4d5f9a0679e04ca5d8d3c
nivida <samuel@ethereum.org>
2021-04-12 09:43:24 +0200

    basic setup for documentation

---
+++ src/.vuepress/config.js
@@ -0,0 +1,52 @@
+const { description } = require('../../package')
+
+module.exports = {
+  title: 'Aragon Documentation',
+  description: description,
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: true,
+    nav: [
+      {
+        text: 'Getting Started',
+        link: '/getting-started/',
+      },
+      {
+        text: 'Govern',
+        link: '/govern/',
+      },
+      {
+        text: 'Court',
+        link: '/court/'
+      },
+      {
+        text: 'Voting',
+        link: '/voting/'
+      }
+    ],
+    sidebar: {
+      '/getting-started/': [
+        {
+          title: 'Getting Started',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: /aragon_logo.svg
+tagline: Aragon V2
+actionText: Getting Started →
+actionLink: /guide/
+features:
+- title: Govern
+  details: Optimistic Governance 
+- title: Court
+  details: Digital dispute resolution made easy
+- title: Voting
+  details: L2 privacy preserving and decentralized voting solution
+footer: Made by Aragon Association with ❤️
+---
